8a00cf38a888904527ffbc02f22ea83c6b52fbdf
samira273 <samiramarassy@gmail.com>
2020-03-24 19:24:41 +0200

    setting up swiftgen, and making a structure

---
+++ Channels/Routing+Management/AppManager.swift
@@ -0,0 +1,14 @@
+//
+//  AppManager.swift
+//  Channels
+//
+//  Created by Samira.Marassy on 3/24/20.
+//  Copyright © 2020 Samira.Marassy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class AppManager {
+    static var shared = AppManager()
+    private init() {}
+}
---
+++ Channels/Routing+Management/AppRouter.swift
@@ -0,0 +1,20 @@
+//
+//  AppRouter.swift
+//  Channels
+//
+//  Created by Samira.Marassy on 3/24/20.
+//  Copyright © 2020 Samira.Marassy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppRouter {
+    // Mark: - variables.
+    static var shared = AppRouter()
+    private var navController: UINavigationController!
+    private var rootViewController: UIViewController!
+    
+    private init() {}
+    
+}
